863525a87ddab3c292bdf8c484686d8ff23e6c16
Andrey Sorokin <sorokindrew@mail.ru>
2023-11-21 15:53:57 +0300

    App created with CRA

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-red-300">
+      <h1 className='font-bold'>My test project</h1>
+    </div>
+  );
+}
+
+export default App;
