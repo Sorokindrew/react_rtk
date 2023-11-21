26a92de36706ed3cd73397da4b204652d4818e88
Andrey Sorokin <sorokindrew@mail.ru>
2023-11-21 17:21:34 +0300

    Prettier and eslint configs added

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="bg-red-300">
-      <h1 className='font-bold'>My test project</h1>
+      <h1 className="font-bold">My test project</h1>
     </div>
   );
 }
